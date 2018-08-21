25d0077f9212226df11f4e169d524c70ded848b5
Pansila <pansila@163.com>
2018-08-21 23:08:48 +0800

    initial UI layout with a navigation bar

---
+++ src/renderer/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">控制台</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item href="#">调试</b-nav-item>
+        <b-nav-item href="#">自动化测试</b-nav-item>
+        <b-nav-item href="#">文档查询</b-nav-item>
+        <b-nav-item href="#">固件下载</b-nav-item>
+        <b-nav-item href="#">固件分析</b-nav-item>
+        <b-nav-item href="#">抓包分析</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+</script>
+
+<style>
+  /* CSS */
+</style>
